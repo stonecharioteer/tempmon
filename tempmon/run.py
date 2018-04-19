dbfc1d666d9dd27afe6a8a918988da30b1d104d8
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2018-04-19 21:01:09 +0530

    Working on the skeleton for the tempmon API.

---
+++ tempmon/run.py
@@ -0,0 +1,5 @@
+from tempmon import app as application
+
+if __name__ == "__main__":
+    application.run("0.0.0.0", port=80)
+    
---
+++ tempmon/tempmon/views.py
@@ -0,0 +1,26 @@
+from flask import jsonify, render_template, redirect
+import requests
+import json
+from expiringdict import ExpiringDict
+from .backend import hosts
+from .tempmon import app
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/tempmon")
+def tempmon():
+    return render_template("tempmon.html")
+
+@app.route("/sensehatpi")
+def sensehatpi():
+    return render_template("sensehatpi.html")
+
+@app.route("/enviropi")
+def enviropi():
+    return render_template("enviropi.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
